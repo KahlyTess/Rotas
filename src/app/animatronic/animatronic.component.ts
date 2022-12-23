7c18c9eb71dce61e07fe925f3c5dfc47716fb93b
KahlyTess <sarahsz204@gmail.com>
2022-12-23 10:34:13 -0300

    subindo alteracoes

--- src/app/animatronic/animatronic.component.ts
+++ src/app/animatronic/animatronic.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AnimatronicService} from "./animatronic.service";
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-animatronic',
@@ -9,12 +11,30 @@ import {AnimatronicService} from "./animatronic.service";
 export class AnimatronicComponent implements OnInit{
 
   animatronics!: any[];
+  pagina!: number;
+  inscricao!: Subscription;
 
-  constructor(private animatronicService: AnimatronicService) {
-  }
+  constructor(
+    private animatronicService: AnimatronicService,
+    private roteador: ActivatedRoute,
+    private router: Router) {}
 
   ngOnInit() {
     this.animatronics = this.animatronicService.getAnimatronics();
+
+      this.inscricao = this.roteador.queryParams.subscribe(
+        (queryParams: any) => {
+          this.pagina = queryParams['pagina']
+        });
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
+  }
+
+  proximaPagina(){
+    this.pagina++;
+    this.router.navigate(['/animatronics']);
   }
 
 }
